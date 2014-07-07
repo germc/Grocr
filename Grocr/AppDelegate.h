ca2912930481028b58af5e1f53e5c4b0bd9f259c
eraofsarah <sarahnagy@Sarahs-MacBook-Pro.local>
2014-07-07 17:23:52 -0400

    Initial Commit

---
+++ Grocr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Grocr
+//
+//  Created by Sarah Nagy on 7/7/14.
+//  Copyright (c) 2014 Sarah Nagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Grocr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Grocr
+//
+//  Created by Sarah Nagy on 7/7/14.
+//  Copyright (c) 2014 Sarah Nagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
